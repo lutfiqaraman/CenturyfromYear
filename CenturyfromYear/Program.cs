dd80a92af09a3505fa44bfc4c354e2edade290e4
lutfiqaraman <lutfiqaraman@gmail.com>
2018-04-12 22:12:16 +0300

    Done

--- CenturyfromYear/Program.cs
+++ CenturyfromYear/Program.cs
@@ -10,6 +10,16 @@ namespace CenturyfromYear
     {
         static void Main(string[] args)
         {
+            int YearofNow = DateTime.Now.Year;
+            int Century = YearofNow / 100;
+            int YearsAfterCentury = YearofNow % 100;
+
+            if (YearsAfterCentury > 0)
+                Century += 1;
+
+            Console.WriteLine("Year: {0}", YearofNow);
+            Console.WriteLine("Century: {0}", Century);
+            Console.ReadKey();
         }
     }
 }
